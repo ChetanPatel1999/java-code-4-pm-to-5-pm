2cf16125e2bf305336d5eeb91c1bd060e45ba066
ChetanPatel1999 <chetan8085patel@gmail.com>
2026-02-27 17:05:33 +0530

    27/02

--- java first project/src/practice.java
+++ java first project/src/practice.java
@@ -5,17 +5,18 @@ public class practice {
         System.out.println("hi am show method");
     }
 
-    void display() {
+    static void display() {
         System.out.println("hi i am display method");
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         System.out.println("hello i am main method");
         practice obj = new practice();
         obj.show();
         System.out.println("inside main method");
-        obj.display();
+        display(); //call without object static method
 
     }
+   
 
 }
